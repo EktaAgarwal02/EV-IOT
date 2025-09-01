0c53fbce2e68196b03963e1ffc51ac8cf2f57985
EktaAgarwal02 <aekta1819@gmail.com>
2025-09-01 15:42:36 +0530

    ....

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template,request,redirect,url_for,flash
+
+app = Flask(__name__)
+@login_manager.user_loader
+def load_user(user_id)
+    return User.query .get(int(user_id))
+df = pd.read_csv('EV_Predictive_Maintenance_Dataset_15min.csv')
+#load the saved model and preprocessing components
+model = joblib.load('xgboost.pkl')
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about') 
+def about():
+    return render_template('about.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
