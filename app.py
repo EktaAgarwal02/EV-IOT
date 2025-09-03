47fcb932e6814624d41b8becb01629d7956509cc
EktaAgarwal02 <aekta1819@gmail.com>
2025-09-03 14:53:42 +0530

    ...

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
-from flask import Flask,render_template,request
-import joblib 
-import 
+from flask import Flask,render_template, request, redirect, url_for, flash
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+from joblib import dump, load # dump is used to save the model and load is used to load the model
 
 app = Flask(__name__)
-@login_manager.user_loader
-def load_user(user_id)
-    return User.query .get(int(user_id))
-df = pd.read_csv('EV_Predictive_Maintenance_Dataset_15min.csv')
-#load the saved model and preprocessing components
+
+
+
+# load the saved model and preprocessing components
 model = joblib.load('xgboost.pkl')
-@app.route('/')
+scaler = joblib.load('scaler.pkl')
+label_encoders = joblib.load('label_encoders.pkl')
+
+@app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
-@app.route('/about') 
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('ab.html')
 
-@app.route('/login')
+@app.route("/login")
 def login():
     return render_template('login.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True)
